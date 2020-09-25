bd05547fe1076f65f1da907a67aae2704580839d
wilmerbaby <wilmerbaby@gmail.com>
2020-09-24 22:42:59 -0400

    added developed files

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-var fs = require("fs");
+// array of questions for user
+const questions = [
+
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
